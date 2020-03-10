17195c7c60a6041cbca0c23cef6e2aadf95880aa
Cris <crisvdnolle@hotmail.com>
2020-03-10 14:42:48 +0100

    Added base code for person detection.
    Base code uses opencv2 for person detection

---
+++ PersonDetector.py
@@ -0,0 +1,33 @@
+import cv2
+from imutils.object_detection import non_max_suppression
+import numpy as np
+
+cap = cv2.VideoCapture(1)
+
+while True:
+    ret, frame = cap.read()
+
+    # set up object for default people detection
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    frame = cv2.resize(frame, (640, 480))
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+    for (xA, yA, xB, yB) in pick:
+        cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+    # out.write(frame.astype('uint8'))
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+# out.release()
+cv2.destroyAllWindows()
