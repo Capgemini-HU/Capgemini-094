3573fd0242a70c3962224591f943ff2be1caba98
Cris <crisvdnolle@hotmail.com>
2020-03-30 15:49:21 +0200

    Added timeout for 30 seconds. Added threads for server.

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import socketserver
+from threading import Thread, Event
+import time
+import sys
 from server import TCPHandler
 
-if __name__ == '__main__':
-    server = socketserver.TCPServer(("", 9999), TCPHandler)
+stop_event = Event()
 
+def timeout():
+    timer = 0
+    while True:
+        timer += 1
+        time.sleep(1)
+        if stop_event.is_set():
+            handle_shutdown()
+            break
+
+
+def handle_shutdown():
+    print("Shutting down server")
+    server.server_close()
+    server.shutdown()
+
+if __name__ == '__main__':
+    try:
+        TCPHandler.set_debug(TCPHandler, debug=sys.argv[1])
+    except IndexError:
+        TCPHandler.set_debug(TCPHandler, debug=0)
+    server = socketserver.TCPServer(("", 9999), TCPHandler)
     # run server
-    server.serve_forever()
+    print("starting server, timeout after 30 seconds.")
+    main_thread = Thread(target=server.serve_forever)
+    main_thread.start()
+
+    second_thread = Thread(target=timeout)
+    second_thread.start()
+    second_thread.join(timeout=30)
+    stop_event.set()
+
+
--- server.py
+++ server.py
@@ -4,19 +4,18 @@ import sys
 # Debug statement. Script can be started with
 # "python server.py 0"
 # "python server.py 1"
-if int(sys.argv[1]) == 1:
-    debug = 1
-else:
-    debug = 0
-
-print("Debug: ", debug)
 
 class TCPHandler(socketserver.BaseRequestHandler):
+    debug = None
     """
     The request handler class for our server.
     """
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        if debug:
+        if self.debug:
             print(self.data)
         self.request.sendall(self.data.upper())
+
+    @staticmethod
+    def set_debug(self, debug=0):
+        self.debug = int(debug)
