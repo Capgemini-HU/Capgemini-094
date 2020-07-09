0e20761d79fa02ae99ac441b8669a8bfe4fe2a89
Cris <crisvdnolle@hotmail.com>
2020-07-09 19:41:12 +0200

    Edited code, edited path where heatmap is saved.

--- create_heatmap.py
+++ create_heatmap.py
@@ -1,6 +1,10 @@
 import csv
 from PIL import Image
 import numpy as np
+import os
+
+if not os.path.exists('src\\main\\webapp'):
+    os.makedirs('src\\main\\webapp')
 
 width = 1920
 height = 1280
@@ -8,15 +12,14 @@ height = 1280
 heatmap_info = np.zeros([height, width, 4], dtype=np.uint8)
 heatmap_info[:,:] = [255, 255, 255, 0]
 
-with open("example_heatmap_points.csv") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        print(f'\t{row[0]} works in the {row[1]} department.')
-        heatmap_info[int(row[0]), int(row[1])] = [255, 0, 0, 255]
-        line_count += 1
-    print(f'Processed {line_count} lines.')
+try:
+    with open("heatmap_points.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            heatmap_info[int(row[0]), int(row[1])] = [255, 0, 0, 255]
 
-
-img = Image.fromarray(heatmap_info)
-img.save('heatmap.png')
+    img = Image.fromarray(heatmap_info)
+    img.save('src\\main\\webapp\\heatmap.png')
+    print("heatmap saved.")
+except FileNotFoundError:
+    print("Image not found, nothing saved")
