b3cd36bca5275bc4b9f619c8548ceb9d13f63a6e
Cris <crisvdnolle@hotmail.com>
2020-07-10 10:31:21 +0200

    Small edits
    Changed check if C:\InnovationFiles exists.
    Changed loading of csv from different path.
    Removed unnecessary code

--- create_heatmap.py
+++ create_heatmap.py
@@ -3,14 +3,12 @@ from PIL import Image
 import numpy as np
 import os
 
-if not os.path.exists('src\\main\\webapp'):
-    os.makedirs('src\\main\\webapp')
-
-print(os.path.dirname(os.path.abspath(__file__)))
+if not os.path.exists('C:\\InnovationFiles\\'):
+    print('not exist')
+    os.makedirs('C:\\InnovationFiles\\')
 
 try:
     base = Image.open('C:\\InnovationFiles\\base_image.png').convert('RGB')
-
 except FileNotFoundError:
     print("Base image not found. Make sure it is in C:\\InnovationFiles and named 'base_image.jpg'")
 
@@ -22,7 +20,7 @@ heatmap_info = np.zeros([height, width, 4], dtype=np.uint8)
 heatmap_info[:,:] = [255, 255, 255, 0]
 
 try:
-    with open("heatmap_points.csv") as csv_file:
+    with open("C:\\InnovationFiles\\heatmap_points.csv") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             heatmap_info[int(row[0]) + 1, int(row[1]) + 1] = [255, 0, 0, 255]
@@ -35,12 +33,8 @@ try:
     img = Image.fromarray(heatmap_info)
     img.convert('RGB')
 
-    # img.paste(im=base, mask=img)
-
     base.paste(im=img, mask=img)
-
     base.save('C:\\InnovationFiles\\heatmap.png')
-    # blend = Image.blend(im1=img, im2=base, alpha=0.5)
     img.save('C:\\InnovationFiles\\heatmap_empty.png')
     print("heatmap saved.")
 except FileNotFoundError:
